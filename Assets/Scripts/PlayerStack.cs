7d93372b954096f7b8a2c904f0575cf4a88307e8
alexiacrawford <alexia.crawford@gmail.com>
2025-04-27 17:53:56 -0700

    adding changes

---
+++ Assets/Scripts/PlayerStack.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerStack : MonoBehaviour {
+    public List<string> playerFlavors = new List<string>();
+    public CustomerOrder currentOrder;
+
+    public void AddFlavor(string flavor) { // adds new scoop to cone
+        playerFlavors.Add(flavor); 
+    } 
+
+    public void TrySubmitOrder() //check player stack against customer order
+    {
+        if (currentOrder.CheckOrder(playerFlavors)){
+            Debug.Log("Correct Order!"); //order matches
+            playerFlavors.Clear();
+            currentOrder.gameObject.SetActive(false); //hide receipt
+        } else {
+            Debug.Log("Incorrect order. Try again."); // order does not match
+        }
+    }
+}
